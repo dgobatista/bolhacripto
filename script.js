7f83d063df155cd910afa42a2b98e32dc1edb009
Diego Batista <dgobatistam@outlook.com>
2024-03-10 23:46:41 -0300

    Adicionei sessão

---
+++ script.js
@@ -0,0 +1,15 @@
+featch('https://api.coincap.io/v2/assets')
+    .then(response => response.json())
+    .then(data => {
+
+
+    })
+
+    function PreencherLista(data){
+        const listaContainer = document.getElementById('lista-container')
+        const ul = document.createElement('ul')
+        data.forEach(iten => {
+            const. li = document.createElement
+            
+        });
+    }
